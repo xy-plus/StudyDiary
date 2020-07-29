2f8066288830f3b6083a71fe58738f2116b6d9c5
刘丰源 <liufy17@mails.tsinghua.edu.cn>
2020-07-30 00:58:47 +0800

    update

--- new_day.py
+++ new_day.py
@@ -6,8 +6,10 @@ import os
 d1 = datetime.datetime.strptime('2020-06-17', "%Y-%m-%d")
 today = datetime.datetime.now().strftime("%Y-%m-%d")
 d2 = datetime.datetime.strptime(today, '%Y-%m-%d')
-delta = d2 - d1
-delta = delta.days
+# delta = d2 - d1
+# delta = delta.days
+f = open('README.md', 'r')
+delta = len(f.readlines()) - 2
 
 os.system('echo \'- [Day %d (%s)](./diary/%s.md)\' >> README.md' %
           (delta, today, today))
